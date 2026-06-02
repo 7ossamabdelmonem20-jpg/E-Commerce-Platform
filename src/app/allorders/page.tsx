d730b008fd57066d3cd361b1a03e8757df762c4b
Hossam_Abogabal22 <7ossamabdelmonem20@gmail.com>
2026-06-02 14:05:07 +0300

    Last modified

---
+++ src/app/allorders/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function AllOrdersPage() {
+  // بيانات وهمية (Mock Data)
+  
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-6 md:p-10">
+      
+      {/* Header */}
+      <div className="mb-10">
+        <h1 className="text-4xl font-bold text-gray-800">
+          My Orders
+        </h1>
+        <p className="text-gray-500 mt-2">
+           Thanks for shopping with us
+        </p>
+      </div>
+
+      
+
+    
+    </div>
+  );
+}
