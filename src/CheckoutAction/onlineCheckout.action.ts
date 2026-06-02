13e4b251e3c70d71a215456c1ba71ad09e02045a
Hossam_Abogabal22 <7ossamabdelmonem20@gmail.com>
2026-06-02 12:56:33 +0300

    final version

--- src/CheckoutAction/onlineCheckout.action.ts
+++ src/CheckoutAction/onlineCheckout.action.ts
@@ -1,25 +1,28 @@
-"use server"
+"use server";
 
-import getMyToken from "@/utilities/getMyToken"
-import { checkoutSchema } from '@/schema/checkout.schema';
+import getMyToken from "@/utilities/getMyToken";
+import type { CheckoutFormValues } from "@/schema/checkout.schema";
 
+export default async function onlinePayment(
+  cartId: string,
+  url: string,
+  formValues: CheckoutFormValues
+) {
+  let token = await getMyToken();
 
-export default async function onlinePayment(cartId:string,url:string,formValuse:checkoutSchema){
-    let token = await getMyToken()
+  if (!token) throw new Error("Login First");
 
-    if(!token){
-        throw new Error ("Login First ")
-    }
-   let res = await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${url}`,{
-    method:"POST",
-    headers:{
+  let res = await fetch(
+    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${url}`,
+    {
+      method: "POST",
+      headers: {
         token,
-        "Content-Type":"application/json"
-    },
-    body:JSON.stringify({shippingAddress:formValuse})
-   })
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ shippingAddress: formValues }),
+    }
+  );
 
-
-   let payload =await  res.json()
-   return payload;
+  return res.json();
 }
--- src/schema/checkout.schema.ts
+++ src/schema/checkout.schema.ts
@@ -1,17 +1,9 @@
-import * as z from "zod";
+import { z } from "zod";
 
 export const checkoutSchema = z.object({
-    details: z
-        .string()
-        .min(3, "Details must be at least 3 characters"),
-
-    phone: z
-        .string()
-        .min(11, "Phone number must be 11 numbers")
-        .max(11, "Phone number must be 11 numbers").regex(/^01[0125][0-9]{8}$/),
-
-    city: z
-        .string()
-        .min(2, "City name is too short"),
-
+  details: z.string(),
+  phone: z.string(),
+  city: z.string(),
 });
+
+export type CheckoutFormValues = z.infer<typeof checkoutSchema>;
