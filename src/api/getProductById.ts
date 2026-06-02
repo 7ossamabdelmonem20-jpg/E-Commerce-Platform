d34cafbe982d0ce78eefe78193a1a62b2432845b
Hossam_Abogabal22 <7ossamabdelmonem20@gmail.com>
2026-06-02 13:01:01 +0300

    Last modified

--- src/api/getProductById.ts
+++ src/api/getProductById.ts
@@ -1,8 +1,7 @@
-export default async function getProductById(id) {
-    let res = await fetch(
-        `https://ecommerce.routemisr.com/api/v1/products/${id}`,
-    );
+export default async function getProductById(id: string) {
+  let res = await fetch(
+    `https://ecommerce.routemisr.com/api/v1/products/${id}`
+  );
 
-    let { data } = await res.json();
-    return data
-} 
+  return res.json();
+}
